2bb4626af428fc57d44240586fa01c944749d533
magmueller <mamagnus00@gmail.com>
2024-11-18 23:51:09 +0100

    Example job application

--- examples/find_and_apply_to_jobs.py
+++ examples/find_and_apply_to_jobs.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import sys
+import time
 from pathlib import Path
 
 from PyPDF2 import PdfReader
@@ -20,8 +21,7 @@ from browser_use.agent.service import Agent
 from browser_use.browser.service import Browser
 from browser_use.controller.service import Controller
 
-# Initialize controller first
-controller = Controller()
+controller = Controller(keep_open=True)
 
 
 class Job(BaseModel):
@@ -29,6 +29,7 @@ class Job(BaseModel):
 	link: str
 	company: str
 	salary: Optional[str] = None
+	location: Optional[str] = None
 
 
 class Jobs(BaseModel):
@@ -43,43 +44,32 @@ def save_jobs(params: Jobs):
 			writer.writerow([job.title, job.company, job.salary, job.link])
 
 
-# read from file
 @controller.action('Read jobs from file')
 def read_jobs():
 	with open('jobs.csv', 'r') as f:
 		return f.read()
 
 
-# Without Pydantic model - using simple parameters
-@controller.action('Ask me for information / help')
+@controller.action('Ask me for help')
 def ask_human(question: str) -> str:
 	return input(f'\n{question}\nInput: ')
 
 
-@controller.action('Read my cv')
+@controller.action('Read my cv for context to fill forms')
 def read_cv():
-	pdf = PdfReader('cv_04_24.pdf')
-
+	pdf = PdfReader('your_cv.pdf')
 	text = ''
-
 	for page in pdf.pages:
 		text += page.extract_text() or ''
-
 	return text
-
-
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 
 @controller.action('Upload cv to index', requires_browser=True)
 def upload_cv(index: int, browser: Browser):
-	ActionChains(browser._get_driver()).send_keys(Keys.ESCAPE).perform()
-	file_input = browser.wait_for_element('input[type="file"]')
-	my_cv = Path.cwd() / 'cv_04_24.pdf'
-
-	file_input.send_keys(my_cv.as_posix())
-
+	close_file_dialog(browser)
+	element = browser.get_element(index)
+	my_cv = Path.cwd() / 'your_cv.pdf'
+	element.send_keys(str(my_cv.absolute()))
 	return f'Uploaded cv to index {index}'
 
 
@@ -89,14 +79,12 @@ def close_file_dialog(browser: Browser):
 
 
 async def main():
-	# task = (
-	# 	'Read my cv & find machine learning engineer jobs in Zurich for me. '
-	# 	'Save them to a file'
-	# 	'then start applying for them in new tabs - please not via job portals like linkedin, indeed, etc. '
-	# 	'If you need more information or help, ask me.'
-	# )
-
-	task = 'go to https://jobs.lever.co/anybotics/edba1c62-9473-4043-8b1d-a353b2527587/apply# and Upload my cv '
+	task = (
+		'Read my cv & find machine learning engineer jobs for me. '
+		'Save them to a file'
+		'then start applying for them in new tabs - please not via job portals like linkedin, indeed, etc. '
+		'If you need more information or help, ask me.'
+	)
 	model = ChatOpenAI(model='gpt-4o')
 	agent = Agent(task=task, llm=model, controller=controller)
 
