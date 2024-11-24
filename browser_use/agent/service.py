9db5f60aa0c72f135c04acff72f56086b1a89c66
magmueller <mamagnus00@gmail.com>
2024-11-24 19:55:38 +0100

    Removed token calculation from agent to simplify

--- browser_use/agent/service.py
+++ browser_use/agent/service.py
@@ -27,9 +27,6 @@ from browser_use.agent.views import (
 	AgentHistory,
 	AgentHistoryList,
 	AgentOutput,
-	ModelPricingCatalog,
-	TokenDetails,
-	TokenUsage,
 )
 from browser_use.browser.views import BrowserState
 from browser_use.controller.service import Controller
@@ -100,14 +97,6 @@ class Agent:
 		if save_conversation_path:
 			logger.info(f'Saving conversation to {save_conversation_path}')
 
-		self.usage_metadata = TokenUsage(
-			input_tokens=0,
-			output_tokens=0,
-			total_tokens=0,
-			input_token_details=TokenDetails(),
-			output_token_details=TokenDetails(),
-		)
-
 	def _setup_action_models(self) -> None:
 		"""Setup dynamic action models from controller's registry"""
 		# Get the dynamic action model from controller's registry
@@ -126,6 +115,8 @@ class Agent:
 			self.message_manager.add_state_message(state, self._last_result)
 			input_messages = self.message_manager.get_messages()
 			model_output = await self.get_next_action(input_messages)
+			self._save_conversation(input_messages, model_output)
+			self.message_manager._remove_last_state_message()  # we dont want the whole state in the chat history
 			self.message_manager.add_model_output(model_output)
 
 			result = await self.controller.act(model_output.action)
@@ -134,7 +125,7 @@ class Agent:
 			if result.extracted_content:
 				logger.info(f'📄 Result: {result.extracted_content}')
 			if result.is_done:
-				logger.info(f'{result.extracted_content}')
+				logger.result(f'{result.extracted_content}')
 
 			self.consecutive_failures = 0
 
@@ -199,136 +190,9 @@ class Agent:
 		parsed: AgentOutput = response['parsed']
 
 		self._log_response(parsed)
-		self._save_conversation(input_messages, parsed)
 		self.n_steps += 1
 
-		# self._update_usage_metadata(response['raw'])
 		return parsed
-
-	def _calc_token_cost(self) -> float:
-		"""
-		Calculate the cost of tokens used in a request based on the model.
-
-		:param usage_metadata: TokenUsage model containing token usage details.
-		:param model_name: The name of the model used.
-		:return: Cost of the tokens used.
-		"""
-		if isinstance(self.llm, ChatOpenAI):
-			model_name = self.llm.model_name
-		elif isinstance(self.llm, ChatAnthropic):
-			model_name = self.llm.model
-		else:
-			logger.debug('Model name not supported for pricing calculation')
-			return 0
-
-		pricing_catalog = ModelPricingCatalog()
-
-		if model_name == 'gpt-4o':
-			model_pricing = pricing_catalog.gpt_4o
-		elif model_name == 'gpt-4o-mini':
-			model_pricing = pricing_catalog.gpt_4o_mini
-		elif model_name == 'claude-3-5-sonnet-20240620':
-			model_pricing = pricing_catalog.claude_3_5_sonnet
-		else:
-			logger.debug(f'Unsupported model: {model_name}')
-			return 0
-
-		uncached_input_tokens = (
-			self.usage_metadata.input_tokens - self.usage_metadata.input_token_details.cache_read
-		)
-		factor = 1e6
-		cost = (
-			(uncached_input_tokens / factor) * model_pricing.uncached_input
-			+ (self.usage_metadata.input_token_details.cache_read / factor)
-			* model_pricing.cached_input
-			+ (self.usage_metadata.output_tokens / factor) * model_pricing.output
-		)
-		return cost
-
-	def _update_usage_metadata(self, raw_response: AIMessage) -> None:
-		"""
-		Process the response and update usage.
-
-		:param raw_response: The response object containing usage metadata.
-		"""
-		# only supported for openai models for now
-		if isinstance(self.llm, ChatAnthropic):
-			token_usage_data: dict[str, Any] = raw_response.response_metadata['usage']
-			usage_metadata = TokenUsage(
-				input_tokens=token_usage_data.get('input_tokens', 0),
-				output_tokens=token_usage_data.get('output_tokens', 0),
-				total_tokens=token_usage_data.get('input_tokens', 0)
-				+ token_usage_data.get('output_tokens', 0),
-			)
-			self.usage_metadata.input_tokens += usage_metadata.input_tokens
-			self.usage_metadata.output_tokens += usage_metadata.output_tokens
-			self.usage_metadata.total_tokens += usage_metadata.total_tokens
-
-		elif isinstance(self.llm, ChatOpenAI):
-			token_usage_data: dict[str, Any] = raw_response.response_metadata['token_usage']
-			usage_metadata = TokenUsage(
-				input_tokens=token_usage_data.get('prompt_tokens', 0),
-				output_tokens=token_usage_data.get('completion_tokens', 0),
-				total_tokens=token_usage_data.get('total_tokens', 0),
-				input_token_details=TokenDetails(
-					audio=token_usage_data.get('prompt_tokens_details', {}).get('audio_tokens', 0),
-					cache_read=token_usage_data.get('prompt_tokens_details', {}).get(
-						'cached_tokens', 0
-					),
-					reasoning=0,  # Assuming reasoning is not part of prompt_tokens_details
-				),
-				output_token_details=TokenDetails(
-					audio=token_usage_data.get('completion_tokens_details', {}).get(
-						'audio_tokens', 0
-					),
-					cache_read=0,  # Assuming cache_read is not part of completion_tokens_details
-					reasoning=token_usage_data.get('completion_tokens_details', {}).get(
-						'reasoning_tokens', 0
-					),
-				),
-			)
-
-			self.usage_metadata.input_tokens += usage_metadata.input_tokens
-			self.usage_metadata.output_tokens += usage_metadata.output_tokens
-			self.usage_metadata.total_tokens += usage_metadata.total_tokens
-
-			# update usage metadata
-			if usage_metadata.input_token_details:
-				for detail_key in usage_metadata.input_token_details.model_dump():
-					setattr(
-						self.usage_metadata.input_token_details,
-						detail_key,
-						getattr(self.usage_metadata.input_token_details, detail_key)
-						+ getattr(usage_metadata.input_token_details, detail_key),
-					)
-
-			if usage_metadata.output_token_details:
-				for detail_key in usage_metadata.output_token_details.model_dump():
-					setattr(
-						self.usage_metadata.output_token_details,
-						detail_key,
-						getattr(self.usage_metadata.output_token_details, detail_key)
-						+ getattr(usage_metadata.output_token_details, detail_key),
-					)
-
-		else:
-			logger.debug('Model name not supported for pricing calculation')
-			return
-
-		self._log_usage_metadata(usage_metadata)
-
-	def _log_usage_metadata(self, current_tokens: Optional[TokenUsage] = None) -> None:
-		"""Log the usage metadata"""
-		total_cost = self._calc_token_cost()
-		total_tokens = self.usage_metadata.total_tokens
-		logger.debug(
-			f'🔢 Total Tokens: input: {self.usage_metadata.input_tokens} (cached: {self.usage_metadata.input_token_details.cache_read}) + output: {self.usage_metadata.output_tokens} = {total_tokens} = ${total_cost:.4f} 💰'
-		)
-
-		if current_tokens:
-			logger.debug(
-				f'🔢 Last  Tokens: input: {current_tokens.input_tokens} (cached: {current_tokens.input_token_details.cache_read}) + output: {current_tokens.output_tokens} = {current_tokens.total_tokens} '
-			)
 
 	def _log_response(self, response: Any) -> None:
 		"""Log the model's response"""
