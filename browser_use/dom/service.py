aaad6694620e83d7b4d3c665e51ca3df19e8d7b7
Gregor Žunič <36313686+gregpr07@users.noreply.github.com>
2024-11-27 18:26:45 +0100

    added dom tree processor

--- browser_use/dom/service.py
+++ browser_use/dom/service.py
@@ -19,6 +19,7 @@ class DomService:
 		self.page = page
 		self.xpath_cache = {}
 
+	# region - Clickable elements
 	async def get_clickable_elements(self, highlight_elements: bool = True) -> DOMState:
 		element_tree = await self._build_dom_tree(highlight_elements)
 		selector_map = self._create_selector_map(element_tree)
@@ -95,3 +96,5 @@ class DomService:
 		element_node.children = children
 
 		return element_node
+
+	# endregion
