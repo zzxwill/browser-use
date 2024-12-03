ed6510963dc9414f41434a5a6c62d4bf917d717b
magmueller <mamagnus00@gmail.com>
2024-12-03 12:27:50 +0100

    Flight example

--- examples/flight_search.py
+++ examples/flight_search.py
@@ -7,6 +7,9 @@ Simple try of the agent.
 import os
 import sys
 
+from browser_use.browser.browser import Browser, BrowserConfig
+from browser_use.browser.context import BrowserContext, BrowserContextConfig
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import asyncio
 
@@ -16,20 +19,23 @@ from browser_use import Agent, AgentHistoryList, Controller
 
 llm = ChatOpenAI(model='gpt-4o')
 # browser = Browser(config=BrowserConfig(headless=False))
-controller = Controller()
 
 agent = Agent(
-	task='Find flights on kayak.com from Zurich to Beijing on 25.12.2024 to 02.02.2025',
+	task=(
+		'Find flights on kayak.com from  Zurich to San Francisco on 25.12.2024 to 02.02.2025, make sure to select the right month '
+	),
 	llm=llm,
-	controller=controller,
-	# browser=browser,
+	validate_output=True,
+	browser_context=BrowserContext(
+		browser=Browser(config=BrowserConfig(headless=False)),
+	),
 )
 
 
 async def main():
-	history: AgentHistoryList = await agent.run(2)
+	history: AgentHistoryList = await agent.run(20)
 	# await controller.browser.close(force=True)
-
+	input('Press Enter to close the browser')
 	# await browser.close()
 
 
