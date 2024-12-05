689e2e7b30d39d66edca64f2fea29f7483cfddbd
magmueller <mamagnus00@gmail.com>
2024-12-05 23:37:17 +0100

    Fix counter

--- browser_use/controller/service.py
+++ browser_use/controller/service.py
@@ -496,7 +496,7 @@ class Controller:
 				)
 				if not new_path_hashes.issubset(cached_path_hashes):
 					# next action requires index but there are new elements on the page
-					logger.info(f'Something new appeared after action {i + 1} / {len(actions)}')
+					logger.info(f'Something new appeared after action {i } / {len(actions)}')
 					break
 
 			results.append(await self.act(action, browser_context))
