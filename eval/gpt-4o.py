76e4c2630d72c0aa313d1f34c8fb53bed86a0e15
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-02-16 02:25:04 +0100

    Update gpt-4o.py

--- eval/gpt-4o.py
+++ eval/gpt-4o.py
@@ -1,36 +1,29 @@
 import asyncio
-
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-
 from browser_use import Agent, Controller
-
 load_dotenv()
-
-
-async def run_agent(task: str):
-	llm = ChatOpenAI(
-		model='gpt-4o',
-		temperature=0.0,
-	)
-	agent = Agent(
-		task=task,
-		llm=llm,
-		include_attributes=[
-			'title',
-			'type',
-			'name',
-			'role',
-			'tabindex',
-			'aria-label',
-			'placeholder',
-			'value',
-			'alt',
-			'aria-expanded',
-			'href',
-		],
-	)
-
-	history = await agent.run()
-	final_response = history.final_result()
-	return final_response, history.history
+async def run_agent(task: str, max_steps: int = 38):
+    llm = ChatOpenAI(
+        model='gpt-4o',
+        temperature=0.0,
+    )
+    agent = Agent(
+        task=task,
+        llm=llm,
+        include_attributes=[
+            'title',
+            'type',
+            'name',
+            'role',
+            'tabindex',
+            'aria-label',
+            'placeholder',
+            'value',
+            'alt',
+            'aria-expanded',
+            'href',
+        ],
+    )
+    result = await agent.run(max_steps=max_steps)
+    return result, result.history
