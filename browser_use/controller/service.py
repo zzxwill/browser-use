b2fb7c42e76522a535777b65b06583f704f33434
mkuts12 <mkuts12@gmail.com>
2025-03-09 18:55:33 +0200

    Adding the strip to markdownify will keep the text but not add the url to the data. This transformed a text of 367277 characters to 48969. Quick fix to a more deep problem

--- browser_use/controller/service.py
+++ browser_use/controller/service.py
@@ -183,7 +183,7 @@ class Controller(Generic[Context]):
 			page = await browser.get_current_page()
 			import markdownify
 
-			content = markdownify.markdownify(await page.content())
+			content = markdownify.markdownify(await page.content(), strip=['a', 'img'])
 
 			prompt = 'Your task is to extract the content of the page. You will be given a page and a goal and you should extract all relevant information around this goal from the page. If the goal is vague, summarize the page. Respond in json format. Extraction goal: {goal}, Page: {page}'
 			template = PromptTemplate(input_variables=['goal', 'page'], template=prompt)
