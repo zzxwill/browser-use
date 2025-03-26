c2332f0e96916ad16615f0356c7677e30d0f342e
Anish Hiranandani <anish.hira@gmail.com>
2025-03-26 15:20:24 -0700

    added required env variables mapping + check env func in utils

---
+++ browser_use/constants.py
@@ -0,0 +1,7 @@
+REQUIRED_ENV_VARIABLES = {
+    "ChatOpenAI": ["OPENAI_API_KEY"],
+    "AzureOpenAI": ["AZURE_ENDPOINT", "AZURE_OPENAI_API_KEY"],
+    "ChatBedrockConverse": ["ANTHROPIC_API_KEY"],
+    "ChatAnthropic": ["ANTHROPIC_API_KEY"],
+    "ChatGoogleGenerativeAI": ["GEMINI_API_KEY"]
+}
