5c83d75a7edde06fad851bc3de87c61b8dcae80f
Anish Hiranandani <anish.hira@gmail.com>
2025-03-27 09:04:05 -0700

    deleted deprecated file

--- browser_use/constants.py
+++
@@ -1,7 +0,0 @@
-REQUIRED_ENV_VARIABLES = {
-    "ChatOpenAI": ["OPENAI_API_KEY"],
-    "AzureOpenAI": ["AZURE_ENDPOINT", "AZURE_OPENAI_API_KEY"],
-    "ChatBedrockConverse": ["ANTHROPIC_API_KEY"],
-    "ChatAnthropic": ["ANTHROPIC_API_KEY"],
-    "ChatGoogleGenerativeAI": ["GEMINI_API_KEY"]
-}
