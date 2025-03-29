597c8c6e84c1cc82904d0da445a672bd3b396244
Deshraj Yadav <deshrajdry@gmail.com>
2025-03-28 18:56:05 -0700

    Incorporate suggestions

--- browser_use/agent/views.py
+++ browser_use/agent/views.py
@@ -69,7 +69,7 @@ class AgentSettings(BaseModel):
 
 	# Procedural memory settings
 	enable_memory: bool = True
-	memory_interval: int = 8
+	memory_interval: int = 10
 	memory_config: Optional[dict] = None
 
 
