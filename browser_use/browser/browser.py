ca20ef496a8188da2c2b43cb369a8c653fabdc3a
Nick Sweeting <git@sweeting.me>
2025-05-19 17:50:25 -0700

    add old files for compat

---
+++ browser_use/browser/browser.py
@@ -0,0 +1,3 @@
+from browser_use.browser.compat import Browser, BrowserConfig, BrowserContext, BrowserContextConfig
+
+__all__ = ['Browser', 'BrowserConfig', 'BrowserContext', 'BrowserContextConfig']
