5ccf02b794c77e7865f43f697e66a1d1ba3914a0
mertunsall <mertunsal1905@gmail.com>
2025-07-12 22:02:56 +0200

    fixes

--- browser_use/agent/views.py
+++ browser_use/agent/views.py
@@ -206,6 +206,7 @@ class AgentOutput(BaseModel):
 			def model_json_schema(cls, **kwargs):
 				schema = super().model_json_schema(**kwargs)
 				del schema['properties']['thinking']
+				schema['required'] = ['evaluation_previous_goal', 'memory', 'next_goal', 'action']
 				return schema
 
 		model = create_model(
@@ -233,6 +234,8 @@ class AgentOutput(BaseModel):
 				del schema['properties']['thinking']
 				del schema['properties']['evaluation_previous_goal']
 				del schema['properties']['next_goal']
+				# Update required fields to only include remaining properties
+				schema['required'] = ['memory', 'action']
 				return schema
 
 		model = create_model(
