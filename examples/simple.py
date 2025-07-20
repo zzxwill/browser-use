d695a5692396be187b854ec1a7055cdee86484e1
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-07-20 23:53:00 +0200

    fix temperature for reasoning models

--- examples/simple.py
+++ examples/simple.py
@@ -10,21 +10,39 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+try:
+	from lmnr import Instruments, Laminar
+
+	Laminar.initialize(project_api_key=os.getenv('LMNR_PROJECT_API_KEY'), disabled_instruments={Instruments.BROWSER_USE})
+except Exception:
+	print('Error initializing Laminar')
+	pass
 
 from browser_use import Agent
 
 # Initialize the model
 llm = ChatOpenAI(
-	model='gpt-4.1-mini',
+	model='o4-mini',
+	temperature=0.2,
+)
+
+# Optimized task with more specific instructions
+task = """
+Navigate to the Qatar Airways homepage and search for flights from Doha to Paris departing in the upcoming week; then list the available fare classes and prices.
+Only use https://www.qatarairways.com/ to achieve the task. Don't go to any other site. The task is achievable with just navigation from this site."""
+
+task = ' call done directly and repeat the word "New" 100 times'
+# Performance optimizations
+agent = Agent(
+	task=task,
+	llm=llm,
 )
 
 
-task = 'Find the founders of browser-use'
-agent = Agent(task=task, llm=llm)
-
-
 async def main():
-	await agent.run()
+	history = await agent.run(max_steps=10)
+	# token usage
+	print(history.usage)
 
 
 if __name__ == '__main__':
